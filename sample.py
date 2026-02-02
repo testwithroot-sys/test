c9bec3b20130d11833c0cb77440e502e07de1a5d
testwithroot-sys <test.withroot@gmail.com>
2026-02-03 08:21:29 +0900

    足し算引き算プログラム
    足し算引き算プログラム

--- sample.py
+++ sample.py
@@ -1,7 +1,10 @@
-# 足し算の例
+# 足し算と引き算の例
 
 a = 3
 b = 5
 
-result = a + b
-print(result)  # 出力: 8
+add_result = a + b
+sub_result = a - b
+
+print(add_result)  # 足し算の出力: 8
+print(sub_result)  # 引き算の出力: -2
